b2a306b07e5e73b619cddf2b602b8c89edf1a565
Minorhddez10 <minorhdez10@gmail.com>
2025-02-14 02:11:24 -0600

    envío de correo al profesor cuando se solicitan horas consulta

--- StudentApp/Models/DAO/AppointmentDAO.cs
+++ StudentApp/Models/DAO/AppointmentDAO.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using StudentApp.Models.Entity;
+using StudentApp.Service;
 
 namespace StudentApp.Models.DAO
 {
@@ -13,6 +14,59 @@ namespace StudentApp.Models.DAO
             _configuration = configuration;
             connectionString = _configuration.GetConnectionString("DefaultConnection");
         }
+
+
+        public int CreateAppointment(Appointment appointment)
+        {
+            int result = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlTransaction transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        using (SqlCommand command = new SqlCommand("CreateAppointment", connection, transaction))
+                        {
+                            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                            command.Parameters.AddWithValue("@Id", Guid.NewGuid().ToString());
+                            command.Parameters.AddWithValue("@Date", appointment.Date);
+                            command.Parameters.AddWithValue("@Mode", appointment.Mode);
+                            command.Parameters.AddWithValue("@Status", "pending");
+                            command.Parameters.AddWithValue("@CourseId", appointment.Course.Id);
+                            command.Parameters.AddWithValue("@StudentId", appointment.User.Id);
+
+                            result = command.ExecuteNonQuery();
+                        }
+
+                        using (SqlCommand command = new SqlCommand("GetProfessorEmail", connection, transaction))
+                        {
+                            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                            command.Parameters.AddWithValue("@CourseId", appointment.Course.Id);
+
+                            var professorEmail = command.ExecuteScalar()?.ToString() ?? string.Empty; 
+
+                            SendEmail.AppointmentEmail(professorEmail);
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch (SqlException)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        /*
         public int CreateAppointment(Appointment appointment)
         {
             int result = 0; 
@@ -48,6 +102,7 @@ namespace StudentApp.Models.DAO
 
             return result;
         }
+        */
 
 
 
