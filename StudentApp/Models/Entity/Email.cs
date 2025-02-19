68c1deb13b92ac6b5be8a2554501e54a18403ed5
Minorhddez10 <minorhdez10@gmail.com>
2025-02-18 18:20:20 -0600

    envío de correo al profesor cuando se crea un appointment

---
+++ StudentApp/Models/Entity/Email.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentApp.Models.Entity
+{
+    public class Email
+    {
+        private string toUser;
+        private string subject;
+        private string content;
+
+        public Email()
+        {
+
+        }
+        public Email(string toUser, string subject, string content)
+        {
+            this.toUser = toUser;
+            this.subject = subject;
+            this.content = content;
+        }
+
+        public string ToUser { get => toUser; set => toUser = value; }
+        public string Subject { get => subject; set => subject = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
